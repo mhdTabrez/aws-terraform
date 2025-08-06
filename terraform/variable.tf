c00a5bb420e1dfb75878fe32992413f6983b1170
mhdTabrez <143933386+mhdTabrez@users.noreply.github.com>
2025-08-06 16:15:45 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -4,7 +4,7 @@
     az                  = ["us-east-1a", "us-east-1b"]
     private_subnet      = ["10.0.0.0/19", "10.0.32.0/19"]
     public_subnet       = ["10.0.64.0/19", "10.0.96.0/19"]
-    DB_subnet       = ["10.0.160.0/19", "10.0.192.0/19"]
+    DB_subnet           = ["10.0.160.0/19", "10.0.192.0/19"]
     private_subnet_tags = {
 
       "kubernetes.io/role/internal-elb"  = 1
@@ -17,6 +17,7 @@
       #"kubernetes.io/cluster/${var.cluster_name_migration}" = "shared"
 
     }
+
 
 
 
