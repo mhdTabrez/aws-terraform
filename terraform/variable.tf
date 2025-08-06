86bebba58eea94721cb20f1b1e4e7b7ede6b5de1
mhdTabrez <143933386+mhdTabrez@users.noreply.github.com>
2025-08-06 16:01:40 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,5 +1,7 @@
-#cidr_block_vpc = 10.0.0.0/16
+az  =  [ "us-east-1a", "us-east-1b" ]
+cidr_block_vpc = 10.0.0.0/16
 public_subnet = 10.0.1.0/24
 private_subnet = 10.0.51.0/24
 DB_subnet = 10.0.101.0/24
+
 
